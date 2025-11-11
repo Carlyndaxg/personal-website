205217f4a81bfe89a4695cc7a21ee94d5ee5dfc7
Carlynda <carlyndaxg@berkeley.edu>
2025-11-10 18:45:26 -0800

    edge compatible cookie

--- middleware.js
+++ middleware.js
@@ -1,17 +1,16 @@
 import { NextResponse } from 'next/server';
-import cookie from 'cookie';
 
-export default function middleware(request) {
-  const cookieHeader = request.headers.get('cookie') || '';
-  const cookies = cookie.parse(cookieHeader);
-  const authCookie = cookies[process.env.PASSWORD_COOKIE_NAME];
+export function middleware(request) {
+  const cookieName = process.env.PASSWORD_COOKIE_NAME || 'auth_token';
+  const authCookie = request.cookies.get(cookieName);
 
   const loginPath = '/login';
   const isLogin = request.nextUrl.pathname.startsWith(loginPath);
-  
+
   if (!authCookie && !isLogin) {
     return NextResponse.redirect(new URL(loginPath, request.url));
   }
+
   return NextResponse.next();
 }
 
