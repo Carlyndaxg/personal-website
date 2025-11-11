6cf84f77bc473af01d4604820a7187cef504b2c4
Carlynda <carlyndaxg@berkeley.edu>
2025-11-10 18:37:59 -0800

    cookies parsing

--- middleware.js
+++ middleware.js
@@ -5,8 +5,10 @@ export default function middleware(request) {
   const cookieHeader = request.headers.get('cookie') || '';
   const cookies = cookie.parse(cookieHeader);
   const authCookie = cookies[process.env.PASSWORD_COOKIE_NAME];
+
   const loginPath = '/login';
   const isLogin = request.nextUrl.pathname.startsWith(loginPath);
+  
   if (!authCookie && !isLogin) {
     return NextResponse.redirect(new URL(loginPath, request.url));
   }
