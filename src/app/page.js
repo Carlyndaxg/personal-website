f88bd62ff0d54874286d03e5522b366ea18843cf
Carlynda <carlyndaxg@berkeley.edu>
2025-11-10 19:15:02 -0800

    Revert "redundant app/page/js removed"
    This reverts commit e1dc3363f3b9471afa2ba65b4443733e02d522d2.

---
+++ src/app/page.js
@@ -0,0 +1,13 @@
+import { cookies } from "next/headers";
+import { redirect } from "next/navigation";
+
+export default async function HomePage() {
+  const cookieStore = await cookies(); 
+  const cookie = cookieStore.get(process.env.PASSWORD_COOKIE_NAME)?.value;
+
+  if (!cookie) {
+    redirect("/login");
+  }
+
+  redirect("/index.html");
+}
