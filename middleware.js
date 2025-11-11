8843d2350cff2d0a822f5aa444465d92f10f1cb2
Carlynda <carlyndaxg@berkeley.edu>
2025-11-10 19:04:10 -0800

    i think i might actually be going insane

--- middleware.js
+++ middleware.js
@@ -1,5 +1,5 @@
 import { NextResponse } from "next/server";
-import cookie from "cookie";
+import * as cookie from "cookie";
 
 export default function middleware(request) {
   const cookieHeader = request.headers.get("cookie") || "";
