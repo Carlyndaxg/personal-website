5291f0f1cf00e9030f8c3c9c9155c29b92c59492
Carlynda <carlyndaxg@berkeley.edu>
2025-11-20 19:26:07 -0800

    server url rewrite

--- server.js
+++ server.js
@@ -1,25 +1,20 @@
 import express from "express";
 import path from "path";
+import { fileURLToPath } from "url";
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 const __dirname = import.meta.dirname;
+const __filename = fileURLToPath(import.meta.url);
 
 // serve static files from the 'public' directory
 app.use(express.static(path.join(__dirname, 'public')));
 
+// change page urls
+
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
-
-app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
-});
-
-// change page urls
-app.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname, 'public', 'index.html'));
-  });
 
 app.get('/projects', function(req, res) {
     res.sendFile(path.join(__dirname, 'public', 'projects.html'));
@@ -28,3 +23,7 @@ app.get('/projects', function(req, res) {
 app.get('/about', function(req, res) {
     res.sendFile(path.join(__dirname, 'public', 'about.html'));
 })
+
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
